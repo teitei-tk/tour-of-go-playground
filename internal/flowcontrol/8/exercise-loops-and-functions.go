5c0401198e825d4fdab2a70a87259af74c1013f0
teitei-tk <teitei.tk@gmail.com>
2020-06-28 01:29:20 +0900

    Exercise: Loops and Functions

---
+++ internal/flowcontrol/8/exercise-loops-and-functions.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := float64(1)
+	for i := 1; i <= 10; i++ {
+		z -= (z*z - x) / (2 * z)
+		fmt.Printf("iterate: %v, value: %v\n", i, z)
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(math.Sqrt(2))
+}
